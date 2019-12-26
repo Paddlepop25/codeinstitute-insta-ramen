0da94fde3270798d7a2dec9760b3b31bee564a52
Ubuntu <ubuntu@ip-172-31-92-186.ec2.internal>
2019-12-26 12:52:22 +0000

    added files to templates folder

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'ramen_database'
 app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-mg1xb.mongodb.net/ramen_database?retryWrites=true&w=majority'
+
 ramen = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_flavours')
 def get_flavours():
-    return render_template('flavours.html', flavours=ramen.db.flavours.find())
+    return render_template('ramen.html', flavours=ramen.db.flavours.find())
 # @app.route('/get_tasks')
 # def get_tasks():
 #     return render_template("tasks.html", tasks=mongo.db.tasks.find())    
