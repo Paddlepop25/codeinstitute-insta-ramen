b7cced75bc63728368a5e744ca1cf849fd371bbe
Ubuntu <ubuntu@ip-172-31-92-186.ec2.internal>
2019-12-26 11:26:02 +0000

    added parent-child ramen htmls

--- app.py
+++ app.py
@@ -5,17 +5,19 @@ from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-# app.config['MONGO_DBNAME'] = 'ramen_database'
-# app.config["MONGO_URI"] = 'mongodb+srv://root:shecodeshsucodes@cluster0-mg1xb.mongodb.net/ramen_database?retryWrites=true&w=majority'
+app.config['MONGO_DBNAME'] = 'ramen_database'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@cluster0-mg1xb.mongodb.net/ramen_database?retryWrites=true&w=majority'
 
-# ramen = PyMongo(app)
+ramen = PyMongo(app)
 
 @app.route('/')
-def ramen():
-    return 'gimme ramen. gimme!'
-# @app.route('/get_flavours')
-# def get_flavours():
-#     return render_template('flavours.html', flavours=ramen.db.flavours.find())
+@app.route('/get_flavours')
+def get_flavours():
+    return render_template('flavours.html', flavours=ramen.db.flavours.find())
+# @app.route('/get_tasks')
+# def get_tasks():
+#     return render_template("tasks.html", tasks=mongo.db.tasks.find())    
+    
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
