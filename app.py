c1f2e63be73ef726cc7f9fa6a449636e6e9eba91
Ubuntu <ubuntu@ip-172-31-92-186.ec2.internal>
2019-12-26 10:46:56 +0000

    changed ramen message test

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# app.config['MONGO_DBNAME'] = 'ramen_database'
+# app.config["MONGO_URI"] = 'mongodb+srv://root:shecodeshsucodes@cluster0-mg1xb.mongodb.net/ramen_database?retryWrites=true&w=majority'
+
+# ramen = PyMongo(app)
+
 @app.route('/')
 def ramen():
-    return 'what is your fav instant noodles?'
+    return 'gimme ramen. gimme'
+# @app.route('/get_flavours')
+# def get_flavours():
+#     return render_template('flavours.html', flavours=ramen.db.flavours.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
