31f9bac0b24cd82e70c69b190534e4010a249c49
Ubuntu <ubuntu@ip-172-31-92-186.ec2.internal>
2020-01-16 19:24:41 +0000

    gave some comments to app.py file

--- app.py
+++ app.py
@@ -7,59 +7,55 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
+# Setting environment and private key
 app.config['MONGO_DBNAME'] = 'ramen_database'
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 
 mongo = PyMongo(app)
 
+# Home page
 @app.route('/')
 def index():
     latest_three=mongo.db.ramens.find().sort([("_id", DESCENDING)]).limit(3)
     return render_template('index.html', title="Home", latest_three=latest_three)
-    
+
+# Display ramen page
 @app.route('/display_ramen/<ramen_id>', methods=['GET'])
 def display_ramen(ramen_id):
     ramen = mongo.db.ramens.find_one({"_id": ObjectId(ramen_id)})
     return render_template('display_ramen.html', title="Display Ramen", ramen=ramen)   
-    
+
+# Ramen Collection page    
 @app.route('/get_ramen')
 def get_ramen():
     ramens=mongo.db.ramens.find().sort([("_id", DESCENDING)])
     return render_template('ramen_collection.html', title="Ramen Collection", ramens=ramens)
     
+# Ramen in Asia page    
 @app.route('/ramen_asia')
 def ramen_asia():
     ramens=mongo.db.ramens.find({'continent': 'Asia'})
     return render_template('ramen_asia.html', title="Ramen from Asia", ramens=ramens)
-    
+
+# Ramen in Europe page    
 @app.route('/ramen_europe')
 def ramen_europe():
     ramens=mongo.db.ramens.find({'continent': 'Europe'})
     return render_template('ramen_europe.html', title="Ramen from Europe", ramens=ramens)
-    
+
+# Ramen in the Americas page    
 @app.route('/ramen_americas')
 def ramen_americas():
     ramens=mongo.db.ramens.find({'continent': 'The Americas'})
     return render_template('ramen_americas.html', title="Ramen from The Americas", ramens=ramens)    
     
+# Ramen in the Rest of the World page    
 @app.route('/ramen_rest_world')
 def ramen_rest_world():
     ramens=mongo.db.ramens.find({'continent': 'The Rest'})
     return render_template('ramen_rest_world.html', title="Ramen from the Rest Of The World", ramens=ramens)    
-    
-# @app.route('/search_ramen')
-# def search_ramen():
-#     orig_query = request.args['query']
-#     query = {'$regex': re.compile('.*{}.*'.format(orig_query)), '$options': 'i'}
-#     print(query)
-#     results=mongo.db.ramens.find({'flavour': query})
-    
-#     ramen = []
-#     for result in results:
-#         ramen.append(result)
-    
-#     return render_template('ramen_search.html', title="Search Results", query=orig_query, ramen_search=ramen)
 
+# Search for ramen flavours by users
 @app.route('/search_ramen/', methods=["GET", "POST"])
 def search_ramen():
     post_request = request.args['query']
@@ -68,13 +64,15 @@ def search_ramen():
                             query=post_request,
                             ramen_search=mongo.db.ramens.find({"flavour" : {"$regex": post_request, "$options": "i"}}),
                             ramen_count=mongo.db.ramens.find({"flavour" : {"$regex": post_request, "$options": "i"}}).count())
-                            
+
+# Add Ramen Page                           
 @app.route('/add_ramen')
 def add_ramen():
     brands = mongo.db.brands.find().sort([("brand", ASCENDING)])
     countries=mongo.db.countries.find().sort([("country", ASCENDING)])
     return render_template('add_ramen.html', title="Add a Ramen", countries=countries, brands=brands)
-    
+
+# Inserting ramen record into database    
 @app.route('/insert_ramen', methods=['POST'])
 def insert_ramen():
     ramens = mongo.db.ramens
@@ -97,10 +95,33 @@ def insert_ramen():
     ramens.insert_one(new_ramen)
     return redirect(url_for('get_ramen'))    
 
+# Uploading of image files
 @app.route('/file/<filename>')
 def file(filename):
     return mongo.send_file(filename)
+
+
+# Add brands page
+@app.route('/add_brands')
+def add_brands():
+    brand = mongo.db.brands.find()
+    add_a_brand = mongo.db.ramens.find()
+    return render_template('add_brands.html', title="Add a Brand", brand=brand)
     
+# Inserting ramen brand into database     
+@app.route('/insert_brand', methods=['POST'])
+def insert_brand():
+    brand = mongo.db.brands
+    query = request.form.get('brand').lower()
+    find_brand = mongo.db.brands.find_one({'brand': query})
+    if find_brand is None:
+        new_brand = {
+            'brand': request.form.get('brand').lower()
+        }
+        brand.insert_one(new_brand)
+    return redirect(url_for('add_ramen'))       
+    
+# Edit ramen page 
 @app.route('/edit_ramen/<ramen_id>', methods=['GET'])
 def edit_ramen(ramen_id):
     ramen = mongo.db.ramens.find_one({"_id": ObjectId(ramen_id)})
@@ -108,6 +129,7 @@ def edit_ramen(ramen_id):
     countries = mongo.db.countries.find().sort([("country", ASCENDING)])
     return render_template('edit_ramen.html', title="Edit Ramen", ramen=ramen, brands=brands, countries=countries)
 
+# Save changes to edit form to database
 @app.route('/update_ramen/<ramen_id>', methods=["POST"])
 def update_ramen(ramen_id):
     ramen = mongo.db.ramens.find_one({"_id": ObjectId(ramen_id)})
@@ -139,30 +161,14 @@ def update_ramen(ramen_id):
         'imageURL': image_filename
     })
     return redirect(url_for('get_ramen'))
-    
+
+# Delete ramen from database
 @app.route('/delete_ramen/<ramen_id>')
 def delete_ramen(ramen_id):
     mongo.db.ramens.remove({'_id': ObjectId(ramen_id)})
     return redirect(url_for('get_ramen'))
 
-@app.route('/add_brands')
-def add_brands():
-    brand = mongo.db.brands.find()
-    add_a_brand = mongo.db.ramens.find()
-    return render_template('add_brands.html', title="Add a Brand", brand=brand)
-    
-@app.route('/insert_brand', methods=['POST'])
-def insert_brand():
-    brand = mongo.db.brands
-    query = request.form.get('brand').lower()
-    find_brand = mongo.db.brands.find_one({'brand': query})
-    if find_brand is None:
-        new_brand = {
-            'brand': request.form.get('brand').lower()
-        }
-        brand.insert_one(new_brand)
-    return redirect(url_for('add_ramen'))       
-    
+# Error 404 page    
 @app.errorhandler(404)
 def error_404(not_found):
     return render_template('error404.html', title="Error 404")
